a013252c887bf70c155ff765c01284ac97289210
guyvinay <mrsinghvinay563@gmail.com>
2026-04-24 10:21:12 +0530

    deploy-all.sh, nexus, formix

--- deploy-all.sh
+++ deploy-all.sh
@@ -6,12 +6,12 @@ echo "**************************************************************************
 echo "Building services..."
 echo "******************************************************************************"
 
-mvn -f $BASE/dev-auth-server/pom.xml clean install -DskipTests
-mvn -f $BASE/dev-record-server/pom.xml clean install -DskipTests
-mvn -f $BASE/dev-form-registry/pom.xml clean install -DskipTests
-mvn -f $BASE/dev-integration/pom.xml clean install -DskipTests
-mvn -f $BASE/dev-sandbox/pom.xml clean install -DskipTests
-mvn -f $BASE/dev-email-server/pom.xml clean install -DskipTests
+mvn -f /home/guyvinay/dev/repo/dev-microservices-lab/dev-auth-server/pom.xml clean install -DskipTests
+mvn -f /home/guyvinay/dev/repo/dev-microservices-lab/dev-record-server/pom.xml clean install -DskipTests
+mvn -f /home/guyvinay/dev/repo/dev-microservices-lab/dev-form-registry/pom.xml clean install -DskipTests
+mvn -f /home/guyvinay/dev/repo/dev-microservices-lab/dev-integration/pom.xml clean install -DskipTests
+mvn -f /home/guyvinay/dev/repo/dev-microservices-lab/dev-sandbox/pom.xml clean install -DskipTests
+mvn -f /home/guyvinay/dev/repo/dev-microservices-lab/dev-email-server/pom.xml clean install -DskipTests
 
 echo "******************************************************************************"
 echo "Building UI..."
@@ -24,12 +24,12 @@ echo "**************************************************************************
 echo "Building Docker images..."
 echo "******************************************************************************"
 
-docker build -t dev-auth-server:main --label project=dev-microservices-lab $BASE/dev-auth-server
-docker build -t dev-record-server:main --label project=dev-microservices-lab $BASE/dev-record-server
-docker build -t dev-form-registry:main --label project=dev-microservices-lab $BASE/dev-form-registry
-docker build -t dev-integration:main --label project=dev-microservices-lab $BASE/dev-integration
-docker build -t dev-sandbox:main --label project=dev-microservices-lab $BASE/dev-sandbox
-docker build -t dev-email-server:main --label project=dev-microservices-lab $BASE/dev-email-server
+docker build -t dev-auth-server:main --label project=dev-microservices-lab /home/guyvinay/dev/repo/dev-microservices-lab/dev-auth-server
+docker build -t dev-record-server:main --label project=dev-microservices-lab /home/guyvinay/dev/repo/dev-microservices-lab/dev-record-server
+docker build -t dev-form-registry:main --label project=dev-microservices-lab /home/guyvinay/dev/repo/dev-microservices-lab/dev-form-registry
+docker build -t dev-integration:main --label project=dev-microservices-lab /home/guyvinay/dev/repo/dev-microservices-lab/dev-integration
+docker build -t dev-sandbox:main --label project=dev-microservices-lab /home/guyvinay/dev/repo/dev-microservices-lab/dev-sandbox
+docker build -t dev-email-server:main --label project=dev-microservices-lab /home/guyvinay/dev/repo/dev-microservices-lab/dev-email-server
 docker build -t formix-ui:main --label project=dev-microservices-lab /home/guyvinay/dev/repo/formix-ui
 
 echo "******************************************************************************"
@@ -51,46 +51,45 @@ echo "Starting deployment at $(date)"
 echo "Deploying Stateful Services..."
 echo "******************************************************************************"
 
-KUBER_BASE=/home/guyvinay/dev/repo/kubernetes
 
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/statefulsets
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/statefulsets/elasticsearch
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/statefulsets/jenkins
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/statefulsets/nexus
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/statefulsets/postgres
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/statefulsets/rabbitmq
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/statefulsets
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/statefulsets/elasticsearch
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/statefulsets/jenkins
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/statefulsets/nexus
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/statefulsets/postgres
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/statefulsets/rabbitmq
 echo "******************************************************************************"
 sleep 2
 
 # ---------- Config ----------
 echo "Applying ConfigMaps and Secrets..."
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/configmap/
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/secret
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/configmap/
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/secret
 echo "******************************************************************************"
 # ---------- Cache ----------
 
 echo "Deploying Redis..."
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/deployment/redis/
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/deployment/redis/
 sleep 2
 # ---------- Microservices ----------
 echo "******************************************************************************"
 echo "Deploying Microservices..."
 
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/deployment/microservices/dev-auth-server/
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/deployment/microservices/dev-record-server/
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/deployment/microservices/dev-form-registry/
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/deployment/microservices/dev-integration/
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/deployment/microservices/dev-auth-server/
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/deployment/microservices/dev-record-server/
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/deployment/microservices/dev-form-registry/
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/deployment/microservices/dev-integration/
 #kubectl apply -R -f ./minikube/root/resources/deployment/microservices/dev-sandbox/
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/deployment/microservices/dev-email-server/
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/deployment/microservices/dev-email-server/
 echo "******************************************************************************"
 # ---------- UI ----------
 
 echo "Deploying UI..."
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/deployment/microservices/formix-ui/
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/deployment/microservices/formix-ui/
 sleep 2
 # ---------- Ingress ----------
 echo "******************************************************************************"
 echo "Deploying Ingress..."
-kubectl apply -R -f $KUBER_BASE/minikube/root/resources/deployment/ingress/
+kubectl apply -R -f /home/guyvinay/dev/repo/kubernetes/minikube/root/resources/deployment/ingress/
 echo "******************************************************************************"
 echo "All services deployed successfully at $(date)"
